243eb5c48719b3409b784d42e17f0c05416c833f
Fabio Ponciroli <ponch78@gmail.com>
2017-12-06 13:04:14 +0000

    Automatically import Kibana dashboards
    Facilitate the spin up of the environment by importing the
    Kibana dashboard and creating the empty gerrit elasticsearch index
    
    Change-Id: I921c9c469d740d79a3f75bd54e7221bfd81c694b

---
+++ dashboard-importer/docker-entrypoint.sh
@@ -0,0 +1,15 @@
+#!/bin/sh
+/wait-for-elasticsearch
+
+echo "** Creating Gerrit index"
+curl -XPUT "http://$ES_USER:$ES_PWD@elasticsearch:9200/gerrit?pretty" -H 'Content-Type: application/json'
+
+echo "** Importing Kibana setting from: "
+for file in `ls -v /*.json`;
+    do  echo "--> $file";
+        /usr/lib/node_modules/elasticdump/bin/elasticdump \
+        --output=http://$ES_USER:$ES_PWD@elasticsearch:9200/.kibana \
+        --input=$file \
+        --type=data \
+        --headers '{"Content-Type": "application/json"}';
+done;
