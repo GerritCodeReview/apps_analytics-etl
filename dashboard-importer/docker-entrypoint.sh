d1fdcad841329196aa2f3e434b656d48ee92ddf7
Tiago Palma <tiago.f.palma@gmail.com>
2018-05-12 17:01:34 +0100

    Create new Kibana reports - Top New Changes
    Kibana reports expect spark to pre-calculate the metrics.
    The reports in Kibana will simply order by priority.
    Elasticsearch mappings will store various metrics, but in this change
    will simply use number of commits and number of patchsets.
    
    - Add Kibana report Top New Changes per NumOfCommits and NumbPatchsets
    - Change docker-entry to deploy mappings
    - Change Dockerfile to include ES mapping files
    
    Resolves Issue #8833
    
    Change-Id: If426f97b5125352242159f1bfdbc8e5c063c3786

--- dashboard-importer/docker-entrypoint.sh
+++ dashboard-importer/docker-entrypoint.sh
@@ -4,8 +4,21 @@
 echo "** Creating Gerrit index"
 curl -XPUT "http://elasticsearch:9200/gerrit?pretty" -H 'Content-Type: application/json'
 
+echo "** Creating gerrit_changes index and deploy change mapping"
+for file in `ls -v /elasticsearch-mappings/*.json`;
+    do
+        echo "--> $file";
+        index_name=$(echo ${file} | cut -d'.' -f1 | cut -d'/' -f2)
+        echo "Index Name: $index_name"
+        /usr/lib/node_modules/elasticdump/bin/elasticdump \
+        --output=http://elasticsearch:9200/$index_name \
+        --input=$file \
+        --type=mapping \
+        --headers '{"Content-Type": "application/json"}';
+done;
+
 echo "** Importing Kibana setting from: "
-for file in `ls -v /*.json`;
+for file in `ls -v /kibana-config/*.json`;
     do  echo "--> $file";
         /usr/lib/node_modules/elasticdump/bin/elasticdump \
         --output=http://elasticsearch:9200/.kibana \
