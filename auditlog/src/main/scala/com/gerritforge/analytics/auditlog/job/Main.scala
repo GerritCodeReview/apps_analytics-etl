6deb46f800172acea70d20e57275100425faab4e
Antonio Barone <syntonyze@gmail.com>
2018-11-29 17:36:53 +0000

    Provide Spark job to process AuditLogs events
    This change provides a new ETL job that allows the processing of auditLog
    events (typically generated by the audit-sl4j plugin).
    Events are normalized, aggregated and then persisted to an elasticsearch
    index. An indended use of this is to then expose this information through
    visualization dashboards, such as kibana.
    
    This work also refactors some general purpose code in order to be shared
    by all the ETLs, such as connection to gerrit APIand time operations.
    
    Issue: Feature 9866
    Change-Id: I9d1fd9ae569de3b5c0d64cb625356fad3a62fd7a

--- auditlog/src/main/scala/com/gerritforge/analytics/auditlog/job/Main.scala
+++ auditlog/src/main/scala/com/gerritforge/analytics/auditlog/job/Main.scala
@@ -1,6 +1,54 @@
-package com.gerritforge.analytics.auditlog.job
-import com.typesafe.scalalogging.LazyLogging
+// Copyright (C) 2018 GerritForge Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
 
-object Main extends App with LazyLogging {
-  // TODO: Implement job here
+package com.gerritforge.analytics.auditlog.job
+
+import com.gerritforge.analytics.auditlog.broadcast.GerritUserIdentifiers
+import com.gerritforge.analytics.auditlog.model.ElasticSearchFields._
+import com.gerritforge.analytics.auditlog.model._
+import com.gerritforge.analytics.auditlog.range.TimeRange
+import com.gerritforge.analytics.auditlog.spark.AuditLogsAggregator
+import com.gerritforge.analytics.common.api.GerritConnectivity
+import com.gerritforge.analytics.spark.SparkApp
+import com.typesafe.scalalogging.LazyLogging
+import org.elasticsearch.spark.sql._
+
+object Main extends SparkApp with App with LazyLogging {
+  override val appName = "Gerrit AuditLog Analytics ETL"
+
+  CommandLineArguments(args) match {
+    case Some(config) =>
+      val tryGerritAccounts = GerritUserIdentifiers.loadAccounts(
+        new GerritConnectivity(config.gerritUsername, config.gerritPassword, config.ignoreSSLCert.getOrElse(false)),
+        config.gerritUrl.get
+      )
+
+      if(tryGerritAccounts.isFailure) {
+        logger.error("Error loading gerrit user identifiers", tryGerritAccounts.failed.get)
+        sys.exit(1)
+      }
+
+      AuditLogsAggregator(tryGerritAccounts.get)
+        .aggregateLogs(
+          logPath         = config.eventsPath.get,
+          timeAggregation = config.eventsTimeAggregation.get,
+          timeRange       = TimeRange(config.since, config.until)
+        )
+        .saveToEs(s"${config.elasticSearchIndex.get}/$DOCUMENT_TYPE")
+    case None =>
+      logger.error("Could not parse command line arguments")
+      sys.exit(1)
+  }
 }
+
