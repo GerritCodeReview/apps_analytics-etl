0b4203c29ce6fd156d4a5ea08c4ffebf782c6256
Antonio Barone <syntonyze@gmail.com>
2018-11-29 17:36:53 +0000

    WIP Update local instance of elasticsearch
    Serialize audit events and update elasticsearch.
    
    Change-Id: I9d1fd9ae569de3b5c0d64cb625356fad3a62fd7a

--- auditlog/src/main/scala/com/gerritforge/analytics/auditlog/job/Main.scala
+++ auditlog/src/main/scala/com/gerritforge/analytics/auditlog/job/Main.scala
@@ -1,6 +1,30 @@
 package com.gerritforge.analytics.auditlog.job
+import com.gerritforge.analytics.auditlog.model.AuditEvent
 import com.typesafe.scalalogging.LazyLogging
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.SparkSession
+import org.elasticsearch.spark.rdd.api.java.JavaEsSpark
 
 object Main extends App with LazyLogging {
-  // TODO: Implement job here
+
+  val eventsPath: String = "/Users/syntonyze/dev/gerrit/audit_log"
+  val elasticSearchHost = "127.0.0.1"
+
+  implicit val spark: SparkSession = SparkSession
+    .builder()
+    .appName("Gerrit AuditLog Analytics ETL")
+    .config("spark.master", "local")
+    .config("spark.es.nodes", elasticSearchHost)
+    .config("spark.es.port", 9200)
+    .config("es.index.auto.create", "true")
+    .getOrCreate()
+
+  val auditJsonStringRDD: RDD[String] = spark
+    .read
+    .textFile(eventsPath)
+    .rdd
+    .flatMap(auditString => AuditEvent.fromJsonString(auditString).toOption)
+    .map(AuditEvent.toJsonString)
+
+  JavaEsSpark.saveJsonToEs(auditJsonStringRDD, resource="gerrit/auditlog")
 }
