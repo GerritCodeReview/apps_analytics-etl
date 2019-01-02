3f0c0d5a0bf0ba62fc13798f5ee9af6bc674fb7e
Fabio Ponciroli <ponch78@gmail.com>
2019-01-02 10:17:43 +0100

    Enrich user info in Auditlogs ETL
    Allow enrichment of user information. This change on allows "user type" to be defined, but it can
    be easily extended to any other useful information.
    
    User data is passed via a CSV file with the following format:
    id,type
    123,bot
    456,human
    
    "User type" is defaulted to "human" if not present in this file.
    
    A possible use case for this feature is the distinction between "bot" and "human" users.
    
    Feature: Issue 10229
    Change-Id: I042244154e668f5137ff97053f1cc2ad88342acd

---
+++ auditlog/src/main/scala/com/gerritforge/analytics/auditlog/broadcast/AdditionalUserInfo.scala
@@ -0,0 +1,45 @@
+package com.gerritforge.analytics.auditlog.broadcast
+
+import com.gerritforge.analytics.auditlog.model.AuditLogETLConfig
+import org.apache.spark.sql.SparkSession
+import org.apache.spark.sql.types.{IntegerType, StringType, StructType}
+
+import scala.util.Try
+
+case class AdditionalUsersInfo(usersInfo: Map[Int,AdditionalUserInfo]) {
+  def getUserType(who: Int): String = usersInfo.get(who).map(_.`type`).getOrElse(AdditionalUserInfo.DEFAULT_USER_TYPE)
+}
+
+object AdditionalUsersInfo {
+  var empty: AdditionalUsersInfo = AdditionalUsersInfo(Map.empty[Int,AdditionalUserInfo])
+}
+
+case class AdditionalUserInfo(id: Int, `type`: String)
+
+object AdditionalUserInfo {
+  val DEFAULT_USER_TYPE = "human"
+
+  def loadAdditionalUserInfo(config: AuditLogETLConfig)(implicit spark: SparkSession): Try[AdditionalUsersInfo] = {
+
+    val schema = new StructType()
+      .add("id", IntegerType,false)
+      .add("type", StringType,false)
+
+    import spark.implicits._
+    Try {
+      AdditionalUsersInfo(
+        config.additionalUserInfoPath.map { path =>
+            spark.read
+              .option("header", "true")
+              .schema(schema)
+              .csv(path)
+              .as[AdditionalUserInfo]
+              // We are collecting on the fair assumption that the additional user info file will fit in memory
+              .collect
+              .map(additionalUserInfo => additionalUserInfo.id -> additionalUserInfo)
+              .toMap
+        }.getOrElse(Map.empty[Int,AdditionalUserInfo])
+      )
+    }
+  }
+}
