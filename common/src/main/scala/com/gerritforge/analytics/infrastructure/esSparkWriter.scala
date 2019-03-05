363135cea67c862df02754275d709ca5bece0416
Tiago Palma <tiago.f.palma@gmail.com>
2019-03-05 12:14:11 +0100

    No downtime when rebuilding ES indices
    Introduce elasticsearch aliases to avoid downtime
    when rebuilding the actual indices.
    
    Feature: Issue 10555
    
    Change-Id: I43477b9d763b28839f002493e8caf78057a8e756

---
+++ common/src/main/scala/com/gerritforge/analytics/infrastructure/esSparkWriter.scala
@@ -0,0 +1,51 @@
+// Copyright (C) 2019 GerritForge Ltd
+//
+// Licensed under the Apache License, Version 2.0 (the "License");
+// you may not use this file except in compliance with the License.
+// You may obtain a copy of the License at
+//
+// http://www.apache.org/licenses/LICENSE-2.0
+//
+// Unless required by applicable law or agreed to in writing, software
+// distributed under the License is distributed on an "AS IS" BASIS,
+// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+// See the License for the specific language governing permissions and
+// limitations under the License.
+
+package com.gerritforge.analytics.infrastructure
+import java.time.{Instant, LocalDateTime, ZoneId}
+
+import com.gerritforge.analytics.common.api.ESRestApiExtraOps
+import com.gerritforge.analytics.support.ops.{AnalyticsDateTimeFormatter, CommonTimeOperations}
+import org.apache.spark.sql.{Dataset, SparkSession}
+import org.elasticsearch.spark.sql._
+
+object ESSparkWriterImplicits {
+  implicit def withAliasSwap[T](data: Dataset[T]): ElasticSearchPimpedWriter[T] =
+    new ElasticSearchPimpedWriter[T](data)
+}
+
+class ElasticSearchPimpedWriter[T](data: Dataset[T]) extends ESRestApiExtraOps {
+
+  override val spark: SparkSession = data.sparkSession
+
+  def saveToESWithAliasSwap(aliasName: String, documentType: String): Unit = {
+
+    val now: Long = Instant.now().toEpochMilli
+    val dateWithStrFormat: String =
+      LocalDateTime
+        .ofInstant(Instant.ofEpochMilli(now), ZoneId.systemDefault())
+        .format(AnalyticsDateTimeFormatter.yyyy_MM_dd)
+
+    val newIndexNameWithTime = s"${aliasName}_${dateWithStrFormat}_$now"
+
+    // Save data
+    data
+      .toDF()
+      .saveToEs(s"$newIndexNameWithTime/$documentType")
+
+    // Replace alias
+    replaceAliasOldIndicesWithNew(aliasName, newIndexNameWithTime)
+
+  }
+}
